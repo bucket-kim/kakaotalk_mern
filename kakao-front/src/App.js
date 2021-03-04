1d21980be1cce9c806a3bae8d5d99b466a5c1601
Brian Kim <brian.kyounghoon.kim@gmail.com>
2021-03-04 11:02:20 +0900

    front-end base setup

---
+++ kakao-front/src/App.js
@@ -0,0 +1,18 @@
+import "./App.css";
+import Chat from "./Chat";
+import Sidebar from "./Sidebar";
+
+function App() {
+  return (
+    <div className="app">
+      <div className="app__body">
+        {/* sidebar */}
+        <Sidebar />
+        {/* chat */}
+        <Chat />
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ kakao-front/src/Sidebar.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Sidebar.css";
+
+function Sidebar() {
+  return (
+    <div className="sidebar">
+      <h1>Sidebar</h1>
+      <div className="sidebar__header"></div>
+      <div className="sidebar__search"></div>
+      <div className="sidebar__chats"></div>
+    </div>
+  );
+}
+
+export default Sidebar;
